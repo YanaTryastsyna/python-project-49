f83307325d37a5b9e3ba3afe3d491ef7d70bb434
Яна Трясцына <yana.tryastsyna@mail.rua1$ git config --global user.email yana.tryastsyna@mail.ru>
2025-06-09 23:27:51 +0300

    add brain_games/scripts/brain_calc.py

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,10 @@
+from brain_games.engine import engine_games
+from brain_games.games.calc import GAME_RULES, generate_round
+
+
+def main():
+    engine_games(GAME_RULES, generate_round)
+
+
+if __name__ == '__main__':
+    main()
