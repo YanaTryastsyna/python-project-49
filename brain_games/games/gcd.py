4e14a757c051bd3a9b0192f6f000a854534041ac
Яна Трясцына <yana.tryastsyna@mail.rua1$ git config --global user.email yana.tryastsyna@mail.ru>
2025-06-11 15:58:35 +0300

    add coomment for sonar

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -10,9 +10,9 @@ def gcd(a, b):
 
 
 def generate_round():
-    # Cryptographic security is not required here
-    number1 = random.randint(1, 100) # nosec B311
-    number2 = random.randint(1, 100) # nosec B311
+    
+    number1 = random.randint(1, 100) #NOSONAR
+    number2 = random.randint(1, 100) #NOSONAR
     question = f"{number1} {number2}"
     answer = str(gcd(number1, number2))
     return question, answer
--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,15 +4,14 @@ GAME_RULES = 'What number is missing in the progression?'
 
 
 def generate_round():
-    # Cryptographic security is not required here
 
-    start = random.randint(1, 100) # nosec B311
-    step = random.randint(1, 10) # nosec B311
-    length = random.randint(5, 15) # nosec B311
+    start = random.randint(1, 100) #NOSONAR
+    step = random.randint(1, 10) #NOSONAR
+    length = random.randint(5, 15) #NOSONAR
 
     progression = [start + step * i for i in range(length)]
 
-    hidden_index = random.randint(0, length - 1) # nosec B311
+    hidden_index = random.randint(0, length - 1) #NOSONAR
     answer = progression[hidden_index]
     progression[hidden_index] = ".."
 
